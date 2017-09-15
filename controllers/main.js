ecb801928d04cff724fbcb5dbf38058f4d9cbd5a
Stuart Moir <stuart.a.moir@live.com>
2017-09-15 21:21:56 +0100

    Adds RR index (untested) to main.js controller

--- controllers/main.js
+++ controllers/main.js
@@ -2,4 +2,31 @@
 var express = require('express');
 var mainRouter = express.Router();
 
+// MongoDB
+require('mongodb').MongoClient;
+
+// RESTful routes.
+
+// Index
+mainRouter.get('/main', function(req, res){
+  MongoClient.connect(url, function(err, db){
+    if (err){console.log('Unable to connect to MongoDB...'); return;}
+    var collection = db.collection('data');
+    collection.find({}).toArray(function(err, docs){
+      res.json(docs); // Send response (array) to BodyParser
+      db.close();
+    });
+  });
+});
+
+// Show
+
+// Edit
+
+// Update
+
+// Create
+
+// Delete
+
 module.exports = mainRouter;
