49e3c760f934f4dc43ed59c23351ced52891f5ca
Ross Melville <rossmelville@hotmail.com>
2017-09-16 14:19:02 +0100

    second test passing

--- client/src/models/specs/term_spec.js
+++ client/src/models/specs/term_spec.js
@@ -30,33 +30,33 @@ describe('Term', function(){
     assert.strictEqual(term.name, "object");
   })
 
-  // it('Should have description', function(){
-  //   assert.strictEqual(term.description, "");
-  // })
+  it('Should have description', function(){
+    assert.strictEqual(term.description, "A self-contained component that contains properties and methods needed to make a certain type of data useful.");
+  })
 
-  // it('Should have keywords', function(){
-  //   assert.strictEqual(term.keywords.length, 2 );
-  // })
+  xit('Should have keywords', function(){
+    assert.strictEqual(term.keywords.length, 2 );
+  })
 
-  // it('Should have image', function(){
-  //   assert.strictEqual(term.image, "./client/build/images/object_vs_class.jpg",);
-  // })
+  xit('Should have image', function(){
+    assert.strictEqual(term.image, "./client/build/images/object_vs_class.jpg",);
+  })
 
-  // it('Should have tags', function(){
-  //   assert.strictEqual(term.tags.length, 2);
-  // })
+  xit('Should have tags', function(){
+    assert.strictEqual(term.tags.length, 2);
+  })
 
-  // it('Should have additional info', function(){
-  //   assert.strictEqual(term.add_info, "Software objects are conceptually similar to real-world objects: An object's properties are what it knows and its methods are what it can do. they too consist of state and related behavior. An object stores its state in fields (variables in some programming languages) and exposes its behavior through methods (functions in some programming languages). Methods operate on an object's internal state and serve as the primary mechanism for object-to-object communication. Hiding internal state and requiring all interaction to be performed through an object's methods is known as data encapsulation — a fundamental principle of object-oriented programming.");
-  // })
+  xit('Should have additional info', function(){
+    assert.strictEqual(term.add_info, "Software objects are conceptually similar to real-world objects: An object's properties are what it knows and its methods are what it can do. they too consist of state and related behavior. An object stores its state in fields (variables in some programming languages) and exposes its behavior through methods (functions in some programming languages). Methods operate on an object's internal state and serve as the primary mechanism for object-to-object communication. Hiding internal state and requiring all interaction to be performed through an object's methods is known as data encapsulation — a fundamental principle of object-oriented programming.");
+  })
 
-  // it('Should have webpages', function(){
-  //   assert.strictEqual(term.webpages.length, 3);
-  // })
+  xit('Should have webpages', function(){
+    assert.strictEqual(term.webpages.length, 3);
+  })
 
-  // it('Should have videos', function(){
-  //   assert.strictEqual(term.videos.length, 3);
-  // })
+  xit('Should have videos', function(){
+    assert.strictEqual(term.videos.length, 3);
+  })
 
 
   
--- client/src/models/term.js
+++ client/src/models/term.js
@@ -1,7 +1,7 @@
 var Term = function(data) {
 
   this.name = data.name; 
-
+  this.description = data.description;
 
 }
 
