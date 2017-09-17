45b09059c3da2f2fdec602344a56161c04bb3514
staceynapier <staceynapier86@gmail.com>
2017-09-17 15:38:10 +0100

    test button linking to getKeyword function - currently in desc_view

--- client/src/views/description_view.js
+++ client/src/views/description_view.js
@@ -18,9 +18,29 @@ DescriptionView.prototype = {
     section.appendChild(pDescription);
 
     var testButton = document.createElement('button');
-    section.appendChild(testButton);
-    testButton.outerHTML = '<button type="button" class="test-button" alt="test understanding">Test Understanding</button>'
-  }
+      section.appendChild(testButton);
+      // Changed class attribute to id attribute.
+      testButton.outerHTML = '<button type="button" id="test-button" alt="test understanding">Test Understanding</button>'
+      // This is new; you need to select the test button separately.
+      button = document.querySelector('#test-button');
+      button.addEventListener('click', function(event){
+        event.preventDefault();
+        console.log("test button clicked");
+        //link from here to another view which allows us to add the fade function
+        var descriptionView = new DescriptionView();
+        descriptionView.getKeyword(term);
+      })
+  },
+
+  getKeyword: function(term) {
+    console.log(term.keywords);
+    var section = document.querySelector('#description-section');
+    var descriptionView = new DescriptionView();
+    descriptionView.fade(section)
+  },
+
+
+
 }
 
 module.exports = DescriptionView;
