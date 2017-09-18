a3774113908e3988d75c2a8522cac56be0036ed6
Craig Lynagh <craiglynagh@gmail.com>
2017-09-18 16:54:56 +0100

    Dropdown syntax update

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -242,25 +242,25 @@ DropdownView.prototype = {
 
   render: function(data){
     console.log(data);
- 
-      var descriptionView = new DescriptionView();
-      var dropDown = document.querySelector("#dropdown-content");
-        while (dropDown.firstChild){ dropDown.removeChild(dropDown.firstChild); };
-      for (var element of data){  
-        var anchor = document.createElement('a')
-        anchor.innerText = element.name;
-        anchor.href = "#"
-        anchor.addEventListener('click', function(e){
-          e.preventDefault();
-          for (var object of data){
-            if (object.name === this.innerText){
-              descriptionView.render(object);
-            }
+    
+    var descriptionView = new DescriptionView();
+    var dropDown = document.querySelector("#dropdown-content");
+    while (dropDown.firstChild){ dropDown.removeChild(dropDown.firstChild); };
+    for (var element of data){  
+      var anchor = document.createElement('a')
+      anchor.innerText = element.name;
+      anchor.href = "#"
+      anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        for (var object of data){
+          if (object.name === this.innerText){
+            descriptionView.render(object);
           }
-          
-        })
-        var section = dropDown.appendChild(anchor)
-      }
+        }
+        
+      })
+      var section = dropDown.appendChild(anchor)
+    }
 
 
       // Close the dropdown menu if the user clicks outside of it
