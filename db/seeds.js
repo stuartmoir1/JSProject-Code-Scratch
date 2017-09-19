9cd74cd4c190eec74924d271af518de30a979236
Ross Melville <rossmelville@hotmail.com>
2017-09-19 23:12:49 +0100

    Removed the image fields from all the seeds.js objects that had it as no longer required.

--- db/seeds.js
+++ db/seeds.js
@@ -166,7 +166,6 @@ db.data.insert([
     "description": "An action that an object is able to perform.",
     "keywords": ["action", "object"],
     "testDescription": ["An ", " that an ", "is able to perform."],
-    "image": "./client/build/images/method.png",
     "tags": ["function", "class", "object"],
     "add_info": "A method defines the behaviour of the objects that are created from the class. Consider the example of an object of the type 'person,' created using the person class. Methods associated with this class could consist of things like walking and driving. Methods are sometimes confused with functions, but they are distinct.",
     "webpages": [
@@ -185,7 +184,6 @@ db.data.insert([
     "description": "A self-contained component that contains properties and methods needed to make a certain type of data useful.",
     "keywords": ["properties", "methods"],
     "testDescription": ["A self-contained component that contains ", " and ", " needed to make a certain type of data useful."],
-    "image": "",
     "tags": ["object-oriented programming", "class"],
     "add_info": "Software objects are conceptually similar to real-world objects: An object’s properties are what it knows and its methods are what it can do. they too consist of state and related behavior. An object stores its state in fields (variables in some programming languages) and exposes its behavior through methods (functions in some programming languages). Methods operate on an object's internal state and serve as the primary mechanism for object-to-object communication. Hiding internal state and requiring all interaction to be performed through an object's methods is known as data encapsulation — a fundamental principle of object-oriented programming.",
     "webpages": [
@@ -205,7 +203,6 @@ db.data.insert([
     "description": "Variables declared within a class.",
     "keywords": ["Variables", "class"],
     "testDescription": ["", "declared within a ", "."],
-    "image": "",
     "tags": ["object-oriented programming", "class"],
     "add_info": "A property, in object-oriented programming languages, is a special sort of class member, intermediate in functionality between a field (or data member) and a method. The syntax for reading and writing of properties is like for fields, but property reads and writes are (usually) translated to 'getter' and 'setter' method calls. The field-like syntax is easier to read and write than lots of method calls, yet the interposition of method calls 'under the hood' allows for data validation, active updating (eg of GUI elements), or implementation of what may be called 'read-only fields'.",
     "webpages": [
@@ -226,7 +223,6 @@ db.data.insert([
     "description": "A variable is a named piece of computer memory, containing some information inside.",
     "keywords": ["variable", "memory", "information"],
     "testDescription": ["A",  "is a named piece of computer ", ", containing some ", " inside."],
-    "image": "",
     "tags": ["object-oriented programming", "array", "integer"],
     "add_info": "A variable is a storage location paired with an associated symbolic name (an identifier), which contains some known or unknown quantity of information referred to as a value. The variable name is the usual way to reference the stored value; this separation of name and content allows the name to be used independently of the exact information it represents. The identifier in computer source code can be bound to a value during run time, and the value of the variable may thus change during the course of program execution.",
     "webpages": [
