be15595d563844fa12252bd281c9a2220ec511a3
Ross Melville <rossmelville@hotmail.com>
2017-09-20 14:23:48 +0100

    Bug fix in seeds.js

--- db/seeds.js
+++ db/seeds.js
@@ -110,7 +110,7 @@ db.data.insert([
     "tags": ["Ruby", "variable"],
     "add_info": "A collection of key-value pairs like this: 'employee' => 'salary'. It is similar to an Array, except that indexing is done via arbitrary keys of any object type, not an integer index.  The order in which you traverse a hash by either key or value may seem arbitrary and will generally not be in the insertion order. If you attempt to access a hash with a key that does not exist, the method will return nil.",
     "webpages": [
-      {"Hash - Ruby (rubylearning.com)": "http://rubylearning.com/satishtalim/ruby_hashes.html"},
+      {"Hash - Ruby (rubylearning)": "http://rubylearning.com/satishtalim/ruby_hashes.html"},
       {"Hash syntax in Ruby (StackOverflow)": "https://stackoverflow.com/questions/4563766/hash-syntax-in-ruby"}
     ],
     "videos": [
